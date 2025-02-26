1358f8b74e20a7066865be1dcedf9a8ca89a86ac
junhan zhang <junhanzhang@Mac.attlocal.net>
2025-02-26 15:39:34 -0800

    add comment to explain

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/") #means when the website goes to "/" route, run the function underneath
 def main():
-    return render_template('app.html',id=None, content=None,message=None)
+    return render_template('app.html',id=None, content=None,message=None) ##render the webpage and send the three variable to front end
 
-@app.route("/post",methods=['GET','POST'])
+@app.route("/post",methods=['GET','POST'])#route() only respond to GET method in default, this make rout() respond to POST method as well
 def post_status():
-    content=request.form.get('content')
+    content=request.form.get('content')#get the frontend's form's input content
     id=post(content)
-    return render_template('app.html',id=id,content=content,message="Post Done!")
+    return render_template('app.html',id=id,content=content,message="Post Done!")#go to the new route and show user "Post Done!"
 
 
 @app.route("/retrieve",methods=['GET','POST'])
@@ -30,5 +30,5 @@ def delete_status():
     content=delete(id)
     return render_template('app.html',id=id,content=content,message=f"status id: {id} delete success")
 
-if __name__=="__main__":
+if __name__=="__main__":#
     app.run(debug=True)
